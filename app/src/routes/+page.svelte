9f4b3ebec8cf0e4e1b74f52d1ed5aa590b861e3b
flessner_ <dev@flessner.io>
2025-02-26 19:11:16 +0100

    first commit

---
+++ app/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { getCurrentWindow } from "@tauri-apps/api/window";
+
+  const window = getCurrentWindow();
+  console.log(window);
+</script>
+
+<main class="container">
+  <h1>Welcome to Tauri + Svelte</h1>
+  <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
+</main>
