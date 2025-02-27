97887ec0ec2186dd4f87b29bcf258135da1ec6b7
flessner_ <dev@flessner.io>
2025-02-27 01:06:54 +0100

    Disabled default window and added a system tray icon

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
+mod tray;
+
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_single_instance::init(|_app, _args, _cwd| {}))
+        .setup(|app| {
+            tray::setup(app)?;
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -2,5 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    althand_lib::run()
+    althand_lib::run();
 }
